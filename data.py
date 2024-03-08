e54501336a2e3db5a3ad4cde2c8e07f49b7f5be8
Daniil Gordienko <danila.gordi@yandex.ru>
2024-03-08 21:27:41 +0300

    first commit

---
+++ data.py
@@ -0,0 +1,8 @@
+class Url:
+    MAIN_PAGE = 'https://stellarburgers.nomoreparties.site/'
+    LOGIN_PAGE = 'https://stellarburgers.nomoreparties.site/login'
+    PROFILE_PAGE = 'https://stellarburgers.nomoreparties.site/account/profile'
+    REGISTER_USER = 'https://stellarburgers.nomoreparties.site/api/auth/register'
+    DELETE_USER = 'https://stellarburgers.nomoreparties.site/api/auth/user'
+    FEED = 'https://stellarburgers.nomoreparties.site/feed'
+
